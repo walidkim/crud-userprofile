4a70de8e225ad026a65f8cf0503421ff4b92a661
walidkim <kimaiyowalid@gmail.com>
2024-05-14 21:56:11 +0300

    initial project structure

---
+++ src/main/java/com/walidkim/cruduserprofile/CrudUserprofileApplication.java
@@ -0,0 +1,13 @@
+package com.walidkim.cruduserprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUserprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUserprofileApplication.class, args);
+	}
+
+}
